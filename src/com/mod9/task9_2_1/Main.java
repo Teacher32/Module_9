5a7c0c60f81bbc320727aa812c792e0600f9ce51
Teacher32 <kitaev-yuri@yandex.ru>
2021-02-03 16:50:47 +0300

    Создан проект с пакетами заданий Модуля 9.

---
+++ src/com/mod9/task9_2_1/Main.java
@@ -0,0 +1,12 @@
+package com.mod9.task9_2_1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(new OneZeroStream());
+        for (int i = 0; i < 2; i++) {
+            System.out.print(scanner.nextInt()); // метод nextInt() считывает и возвращает число
+        }
+    }
+}
---
+++ src/com/mod9/task9_2_1/OneZeroStream.java
@@ -0,0 +1,16 @@
+package com.mod9.task9_2_1;
+
+import java.io.InputStream;
+
+public class OneZeroStream extends InputStream {
+
+    private byte[] bytes = {'1', ' ', '0', ' '};
+    int i = 0;
+
+    @Override
+    public int read() {
+        if (i >= bytes.length)
+            i = 0;
+        return bytes[i++];
+    }
+}
